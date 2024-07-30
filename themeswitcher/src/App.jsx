b4858ecb8b05f79b26d960463fefcac29c9a5637
SMARTRON7 <smartabbas07@gmail.com>
2024-07-31 02:20:46 +0530

    Theme Switcher and part 1 end

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,21 +1,45 @@
 
+import { useState, useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
+
 
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-        <div className="mb-4">
-          {/* Todo form goes here */}
-        </div>
-        <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
-    </div>
+
+    </ThemeProvider>
   )
 }
 
